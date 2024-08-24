f8b8cc3bdd5f886a05d2cf7079841812531bb520
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2024-08-24 20:20:56 -0300

    posts table

--- images/postgres/01_schema.sql
+++ images/postgres/01_schema.sql
@@ -5,3 +5,11 @@ CREATE TABLE users (
 		username VARCHAR(16) UNIQUE NOT NULL,
 		password TEXT NOT NULL
 );
+
+CREATE TABLE posts (
+        id         UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+		post_owner VARCHAR(16) NOT NULL,
+		content    TEXT NOT NULL,
+		created_at TIMESTAMP DEFAULT NOW(),
+		FOREIGN KEY (post_owner) REFERENCES users(username) ON DELETE CASCADE
+);
