3705f632030a2541cffce5c29aa3c4e668246c64
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2024-08-23 21:58:32 -0300

    login flow

--- auth/main.go
+++ auth/main.go
@@ -8,6 +8,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/fnacarellidev/microsservices/.sqlcbuild/pgquery"
@@ -18,24 +19,41 @@ import (
 	"golang.org/x/crypto/bcrypt"
 )
 
+const DbUrl = "postgresql://postgres:postgres@localhost:5432"
+
 type User struct {
 	Username string `json:"username"`
 	Password string `json:"password"`
 }
 
-func generateToken(user User) (interface{}, error) {
+func validateToken(tok string) (*jwt.Token, error) {
 	secret, err := os.ReadFile("auth/hs256secret.txt")
 	if err != nil {
 		return nil, fmt.Errorf("[generateToken] read file err %v\n", err)
 	}
 
+	token, err := jwt.Parse(tok, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+		return secret, nil
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return token, nil
+}
+
+func generateToken(user User) (string, error) {
+	secret, _ := os.ReadFile("auth/hs256secret.txt")
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
 		"username": user.Username,
 		"iat": time.Now().Unix(),
 	})
 	tokenString, err := token.SignedString(secret)
 	if err != nil {
-		return nil, fmt.Errorf("[generateToken] sign token err %v\n", err)
+		return "", fmt.Errorf("[generateToken] sign token err %v\n", err)
 	}
 
 	return tokenString, nil
@@ -44,7 +62,7 @@ func generateToken(user User) (interface{}, error) {
 func Register(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	var user User
 	ctx := context.Background()
-	conn, err := pgx.Connect(ctx, "postgresql://postgres:postgres@localhost:5432")
+	conn, err := pgx.Connect(ctx, DbUrl)
 	if err != nil {
 		log.Println("Failed at db connection:", err)
 		w.WriteHeader(http.StatusInternalServerError)
@@ -90,12 +108,47 @@ func Register(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 		return
 	}
 
-	w.WriteHeader(http.StatusOK)
-	return
+	token, err := generateToken(user)
+	if err != nil {
+		log.Println("Failed to generate token:", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.Write([]byte(token))
+}
+
+func Login(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	ctx := context.Background()
+	_, err := pgx.Connect(ctx, DbUrl)
+	if err != nil {
+		log.Println("Failed at db connection:", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	jwtBearer, found := strings.CutPrefix(r.Header["Authorization"][0], "Bearer ")
+	if !found {
+		w.Write([]byte("No JWT provided"))
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
+	token, err := validateToken(jwtBearer)
+	if err != nil {
+		w.Write([]byte("Unauthorized"))
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
+	if claims, ok := token.Claims.(jwt.MapClaims); ok {
+		log.Println("Logged in successfully as", claims["username"])
+	}
 }
 
 func main() {
 	router := httprouter.New()
-	router.GET("/register", Register)
+	router.GET("/auth/register", Register)
+	router.GET("/auth/login", Login)
 	http.ListenAndServe(":8080", router)
 }
